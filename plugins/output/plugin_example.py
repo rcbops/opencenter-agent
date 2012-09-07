f2604900ea00f1bf4008dfafbfaf7ba6873f9663
wilk <william.kelly@rackspace.com>
2012-09-07 08:45:51 -0500

    pep8

--- plugins/output/plugin_example.py
+++ plugins/output/plugin_example.py
@@ -2,15 +2,17 @@
 
 name = 'example'
 
+
 def setup(config={}):
     LOG.debug('Doing setup in test.py')
     register_action('test', handle_test)
+
 
 def handle_test(input_data):
     payload = input_data['payload']
     action = input_data['action']
 
     print 'Handling action "%s" for payload "%s"' % (action, payload)
-    return { 'result_code': 0,
+    return {'result_code': 0,
              'result_str': 'success',
-             'result_data': None }
+             'result_data': None}
