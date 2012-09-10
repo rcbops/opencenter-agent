42ff5f32b3f2f37cd05a4820385bbcd6e7d0add8
wilk <william.kelly@rackspace.com>
2012-09-10 16:52:48 -0500

    Another approach to passing configed stuff

--- roushagent/plugins/output/plugin_chef.py
+++ roushagent/plugins/output/plugin_chef.py
@@ -11,26 +11,31 @@ def setup(config={}):
     plugin_dir = config.get("plugin_dir", "roushagent/plugins")
     script_path = [os.path.join(plugin_dir, "lib", name)]
     script = BashScriptRunner(script_path=script_path)
-    register_action('install_chef', lambda x: install_chef(x, script))
-    register_action('run_chef', lambda x: run_chef(x, script))
+    chef = ChefThing(script, config)
+    register_action('install_chef', chef.install_chef)
+    register_action('run_chef', chef.run_chef)
 
 
-def install_chef(input_data, script):
-    payload = input_data['payload']
-    action = input_data['action']
-    required = ["CHEF_SERVER", "CHEF_VALIDATOR"]
-    optional = ["CHEF_RUNLIST", "CHEF_ENVIRONMENT", "CHEF_VALIDATION_NAME"]
-    env = dict([(k, v) for k, v in payload.iteritems()
-                if k in required + optional])
-    for r in required:
-        if not r in env:
-            return {'result_code': 22,
-                    'result_str': 'Bad Request (missing %s)' % r,
-                    'result_data': None}
-    return script.run_env("install-chef.sh", env, "")
+class ChefThing(object):
+    def __init__(self, script, config):
+        self.script = script
+        self.config = config
+        
+    def install_chef(self, input_data):
+        payload = input_data['payload']
+        action = input_data['action']
+        required = ["CHEF_SERVER", "CHEF_VALIDATOR"]
+        optional = ["CHEF_RUNLIST", "CHEF_ENVIRONMENT", "CHEF_VALIDATION_NAME"]
+        env = dict([(k, v) for k, v in payload.iteritems()
+                    if k in required + optional])
+        for r in required:
+            if not r in env:
+                return {'result_code': 22,
+                        'result_str': 'Bad Request (missing %s)' % r,
+                        'result_data': None}
+        return self.script.run_env("install-chef.sh", env, "")
 
-
-def run_chef(input_data, script):
-    payload = input_data['payload']
-    action = input_data['action']
-    return script.run("run-chef.sh")
+    def run_chef(self, input_data):
+        payload = input_data['payload']
+        action = input_data['action']
+        return self.script.run("run-chef.sh")
