7e31c35a70580ea9692b6c270cf2688f66f59a4b
wilk <william.kelly@rackspace.com>
2012-09-11 16:23:46 -0500

    First pass at chef server installation

--- roushagent/plugins/output/plugin_chef.py
+++ roushagent/plugins/output/plugin_chef.py
@@ -16,6 +16,23 @@ def setup(config={}):
     register_action('install_chef', chef.install_chef)
     register_action('run_chef', chef.run_chef)
 
+def get_environment(required, optional, payload):
+    env = dict([(k, v) for k, v in payload.iteritems()
+                if k in required + optional])
+    for r in required:
+        if not r in env:
+            return False, {'result_code': 22,
+                           'result_str': 'Bad Request (missing %s)' % r,
+                           'result_data': None}
+    return True, env
+
+def retval(result_code, result_str, result_data):
+    return {'result_code': result_code,
+            'result_str': result_str,
+            'result_data': result_data}
+
+def success(result_str="success", result_data=None):
+    return retval(0, result_str, result_data)
 
 class ChefThing(object):
     def __init__(self, script, config):
@@ -27,16 +44,27 @@ class ChefThing(object):
         action = input_data['action']
         required = ["CHEF_SERVER", "CHEF_VALIDATOR"]
         optional = ["CHEF_RUNLIST", "CHEF_ENVIRONMENT", "CHEF_VALIDATION_NAME"]
-        env = dict([(k, v) for k, v in payload.iteritems()
-                    if k in required + optional])
-        for r in required:
-            if not r in env:
-                return {'result_code': 22,
-                        'result_str': 'Bad Request (missing %s)' % r,
-                        'result_data': None}
+        success, env = get_environment(required, optional, payload)
+        return env if not success
         return self.script.run_env("install-chef.sh", env, "")
 
     def run_chef(self, input_data):
         payload = input_data['payload']
         action = input_data['action']
         return self.script.run("run-chef.sh")
+
+    def install_chef_server(self, input_data):
+        payload = input_data['payload']
+        action = input_data['action']
+        success, env = get_environment([], 
+                                       ["CHEF_URL", "CHEF_WEBUI_PASSWORD" ],
+                                       payload)
+        return env if not success
+        return self.script.run_env("install-chef-server.sh", env, "")
+
+    def get_validation_pem(self, input_data):
+        try:
+            with f as open("/etc/chef/validation.pem", "r"):
+                return success("Success", f.read())
+        except IOError as e:
+            return retval(e.errno, str(e), None)
