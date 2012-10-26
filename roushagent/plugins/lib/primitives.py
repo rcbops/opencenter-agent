f64ae4e331524f5ad1291b81fec6f8e20d2ba54b
wilk <william.kelly@rackspace.com>
2012-10-26 13:50:05 -0500

    pep8

--- roushagent/plugins/lib/primitives.py
+++ roushagent/plugins/lib/primitives.py
@@ -8,9 +8,11 @@ import logging
 from roushclient.client import RoushEndpoint
 from state import StateMachine, StateMachineState
 
+
 # primitive functions for orchestration.
 class OrchestratorTasks:
-    def __init__(self, endpoint='http://localhost:8080', logger=None, parent_task_id=None):
+    def __init__(self, endpoint='http://localhost:8080', logger=None,
+                 parent_task_id=None):
         self.endpoint = RoushEndpoint(endpoint)
         self.logger = logger
         self.parent_task_id = parent_task_id
@@ -22,10 +24,11 @@ class OrchestratorTasks:
             input_state['rollback_plan'] = {}
 
         if not node_id in input_state['rollback_plan']:
-            input_state['rollback_plan'][node_id] = {'start_state': 'state_1',
-                                                     'states': {'state_1':
-                                                                {'primitive': 'noop',
-                                                                 'parameters': {}}}}
+            input_state['rollback_plan'][node_id] = {
+                'start_state': 'state_1',
+                'states': {'state_1':
+                           {'primitive': 'noop',
+                            'parameters': {}}}}
         # we have a rollback plan already
         plan = input_state['rollback_plan'][node_id]
 
@@ -40,7 +43,8 @@ class OrchestratorTasks:
         plan['states'][state] = state_value
         input_state['rollback_plan'][node_id] = plan
 
-        self.logger.debug('new rollback plan for node %s: %s' % (node_id, plan))
+        self.logger.debug('new rollback plan for node %s: %s' % (node_id,
+                                                                 plan))
         return input_state
 
     def sm_eval(self, sm_dict, input_state):
@@ -63,11 +67,11 @@ class OrchestratorTasks:
 
             if not hasattr(self, 'primitive_%s' % action):
                 self.logger.debug('cannot find primitive "%s"' % action)
-                return ({ 'result_code': 1,
+                return ({'result_code': 1,
                           'result_str': 'cannot find primitive "%s"' % action,
-                          'result_data': {}},{})
+                          'result_data': {}}, {})
 
-            fn = otask(getattr(self,'primitive_%s' % action), **parameters)
+            fn = otask(getattr(self, 'primitive_%s' % action), **parameters)
 
             sm.add_state(state, StateMachineState(advance=fn, **vals))
 
@@ -77,9 +81,11 @@ class OrchestratorTasks:
     def primitive_noop(self, state_data):
         return self._success(state_data)
 
-    def primitive_set_backend(self, state_data, backend=None, backend_state=None):
+    def primitive_set_backend(self, state_data,
+                              backend=None, backend_state=None):
         for node in state_data['nodes']:
-            self.logger.debug('Changing node %s backend to %s/%s' % (node, backend, backend_state))
+            self.logger.debug('Changing node %s backend to %s/%s' % (
+                node, backend, backend_state))
             node_obj = self.endpoint.nodes[node]
 
             old_backend = node_obj.backend
@@ -94,10 +100,10 @@ class OrchestratorTasks:
 
             node_obj.save()
 
-            state_data = self.add_rollback_step(node, state_data,
-                                                {'primitive': 'set_backend',
-                                                 'parameters': {'backend': old_backend,
-                                                                'backend_state': old_backend_state}})
+            state_data = self.add_rollback_step(node, state_data, {
+                'primitive': 'set_backend',
+                'parameters': {'backend': old_backend,
+                               'backend_state': old_backend_state}})
         return self._success(state_data)
 
     def primitive_log(self, state_data, msg='default msg'):
@@ -113,9 +119,9 @@ class OrchestratorTasks:
             node_obj.cluster_id = cluster_id
             node_obj.save()
 
-            state_data = self.add_rollback_step(node, state_data,
-                                                {'primitive': 'set_cluster',
-                                                 'parameters': {'cluster_id': old_cluster_id}})
+            state_data = self.add_rollback_step(node, state_data, {
+                'primitive': 'set_cluster',
+                'parameters': {'cluster_id': old_cluster_id}})
 
         return self._success(state_data)
 
@@ -126,9 +132,9 @@ class OrchestratorTasks:
             node_obj.role = role
             node_obj.save()
 
-            state_data = self.add_rollback_step(node, state_data,
-                                                {'primitive': 'set_role',
-                                                 'parameters': {'role': old_role}})
+            state_data = self.add_rollback_step(node, state_data, {
+                'primitive': 'set_role',
+                'parameters': {'role': old_role}})
 
         return self._success(state_data)
 
@@ -143,16 +149,19 @@ class OrchestratorTasks:
             node_obj.config[fact] = value
             node_obj.save()
 
-            state_data = self.add_rollback_step(node, state_data,
-                                                {'primitive': 'set_fact',
-                                                 'parameters': {'fact': fact,
-                                                                'value': old_value}})
+            state_data = self.add_rollback_step(node, state_data, {
+                'primitive': 'set_fact',
+                'parameters': {'fact': fact,
+                               'value': old_value}})
 
         return self._success(state_data)
 
-    def primitive_run_task(self, state_data, action, payload={}, timeout=3600, poll_interval=5):
+    def primitive_run_task(self, state_data, action, payload={},
+                           timeout=3600, poll_interval=5):
         if not 'nodes' in state_data:
-            return self._failure(state_data, result_str='no node list for primitive "run_task"')
+            return self._failure(
+                state_data,
+                result_str='no node list for primitive "run_task"')
 
         result_list = {}
         output_list = {}
@@ -160,12 +169,14 @@ class OrchestratorTasks:
         for node in state_data['nodes']:
             task_id = self._submit_task(state_data, node, action, payload)
             if task_id:
-                self.logger.debug('Submitted task %d on node %d' % (task_id, node))
+                self.logger.debug('Submitted task %d on node %d' % (task_id,
+                                                                    node))
                 result_list[node] = task_id
             else:
                 self.logger.debug('Could not submit task on node %d' % (node,))
 
-                output_list[node] = self._failure(0, result_str='could not submit task')
+                output_list[node] = self._failure(
+                    0, result_str='could not submit task')
 
                 # take this node out of the run list
                 self._fail_node(state_data, node)
@@ -173,7 +184,9 @@ class OrchestratorTasks:
         # wait for all the submitted tasks
         self.logger.debug('Waiting for submitted tasks')
 
-        success_tasks, fail_tasks = self._wait_for_tasks(result_list, timeout, poll_interval)
+        success_tasks, fail_tasks = self._wait_for_tasks(result_list,
+                                                         timeout,
+                                                         poll_interval)
 
         for node in fail_tasks.keys():
             output_list[node] = fail_tasks[node]['result']
@@ -184,33 +197,46 @@ class OrchestratorTasks:
 
         # should append to the run log
         if len(state_data['nodes']) > 0:
-            return self._success(state_data, result_str='task runner succeeded', result_data=output_list)
+            return self._success(state_data,
+                                 result_str='task runner succeeded',
+                                 result_data=output_list)
 
-        return self._failure(state_data, result_str='no successful task executions.', result_data=output_list)
+        return self._failure(state_data,
+                             result_str='no successful task executions.',
+                             result_data=output_list)
 
     def primitive_install_chef(self, state_data):
-        # strategy: find the chef server, run the "get_validation_pem" task, and then
+        # strategy: find the chef server, run "get_validation_pem" task, then
         # run the install_chef task on the nodes with the found validation pem
-        chef_server = self.endpoint.nodes.filter("backend='chef-server'").first()
+        chef_server = self.endpoint.nodes.filter(
+            "backend='chef-server'").first()
         if not chef_server:
-            return self._failure(state_data, result_str='cannot find a chef server')
+            return self._failure(state_data,
+                                 result_str='cannot find a chef server')
 
-        task_result, _ = self.primitive_run_task({'nodes': [chef_server.id]}, action='get_chef_info')
+        task_result, _ = self.primitive_run_task({'nodes': [chef_server.id]},
+                                                 action='get_chef_info')
         if task_result['result_code'] != 0:
-            return self._failure(state_data, result_str='could not get chef info from server')
+            return self._failure(
+                state_data,
+                result_str='could not get chef info from server')
 
-        validation_pem = task_result['result_data'][chef_server.id]['result_data']['validation_pem']
-        chef_endpoint = task_result['result_data'][chef_server.id]['result_data']['chef_endpoint']
+        validation_pem = task_result['result_data'][chef_server.id][
+            'result_data']['validation_pem']
+        chef_endpoint = task_result['result_data'][chef_server.id][
+            'result_data']['chef_endpoint']
 
         # now that we have the info we need, we'll go ahead and run the job
         return self.primitive_run_task(state_data, 'install_chef',
-                                       payload={'CHEF_SERVER': chef_endpoint,
-                                                'CHEF_VALIDATOR':  validation_pem})
+                                       payload={
+                                           'CHEF_SERVER': chef_endpoint,
+                                           'CHEF_VALIDATOR':  validation_pem})
 
     def _success(self, state_data, result_str='success', result_data={}):
         return self._result(state_data, 0, result_str, result_data)
 
-    def _failure(self, state_data, result_code=1, result_str='failure', result_data={}):
+    def _failure(self, state_data, result_code=1,
+                 result_str='failure', result_data={}):
         return self._result(state_data, result_code, result_str, result_data)
 
     def _result(self, state_data, result_code, result_str, result_data):
@@ -234,10 +260,10 @@ class OrchestratorTasks:
         new_task.node_id = node_id
         new_task.parent_id = self.parent_task_id
 
-        state_data = self.add_rollback_step(node_id, state_data,
-                                            {'primitive': 'run_task',
-                                             'parameters': {'action': 'rollback_%s' % action,
-                                                            'payload': payload}})
+        state_data = self.add_rollback_step(node_id, state_data, {
+            'primitive': 'run_task',
+            'parameters': {'action': 'rollback_%s' % action,
+                           'payload': payload}})
 
         new_task.save()
         self.logger.debug('submitting "%s" task as %d' % (action, new_task.id))
@@ -266,10 +292,13 @@ class OrchestratorTasks:
                     # task is done.
                     if task_obj.state in ['timeout', 'cancelled']:
                         # uh oh, that's bad.
-                        failed_tasks[node] = {'task': task,
-                                              'result': {'result_code': 1,
-                                                         'result_msg': 'Task aborted: %s' % task_obj.state,
-                                                         'result_data': {}}}
+                        failed_tasks[node] = {
+                            'task': task,
+                            'result': {
+                                'result_code': 1,
+                                'result_msg': 'Task aborted: %s' % (
+                                    task_obj.state),
+                                'result_data': {}}}
                     else:
                         # done
                         self.logger.debug('Task %d completed' % task)
@@ -277,7 +306,7 @@ class OrchestratorTasks:
                         self.logger.debug('Task output: %s' % result)
 
                         result_entry = {'task': task,
-                                        'result': result }
+                                        'result': result}
 
                         if result['result_code'] == 0:
                             success_tasks[node] = result_entry
