dcbe22e5fa56c2a93a56c052d2c190d625cdd2eb
Joseph W. Breu <breu@breu.org>
2012-12-04 17:51:46 +0000

    initial commit of a package task for roush

---
+++ roushagent/plugins/lib/packages/update-package.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+set -e
+set -u
+set -x
+
+PACKAGE_NAME=${PACKAGE_NAME:-}
+RETVAL=0
+
+if [[ -f /etc/redhat-release ]]; then
+    DISTRO="redhat"
+else
+    DISTRO="debian"
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+function do_single_package() {
+    if [[ $DISTRO = "debian" ]]; then
+        apt-get install ${PACKAGE_NAME}
+        RETVAL=$?
+    else
+        yum -y install ${PACKAGE_NAME}
+        RETVAL=$?
+    fi
+}
+
+function do_update() {
+    if [[ $DISTRO = "debian" ]]; then
+        apt-get upgrade
+        RETVAL=$?
+    else
+        yum -y upgrade
+        RETVAL=$?
+    fi
+}
+
+if [[ -z $PACKAGE_NAME ]]; then
+	do_update
+else
+	do_single_package
+fi
+
+exit $RETVAL
