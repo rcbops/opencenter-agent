44eb4ef3b8b4fcbd278bb32650fd960466034872
rpedde <ron@pedde.com>
2013-01-16 15:59:44 -0600

    pep8, how I loathe you.

--- roushagent/plugins/output/plugin_sleep.py
+++ roushagent/plugins/output/plugin_sleep.py
@@ -9,9 +9,8 @@ name = 'sleep'
 
 def setup(config={}):
     LOG.debug('doing setup for sleep handler')
-    register_action('sleep', handle_sleep, args={"sleep_interval": {
-                "required": True, "type": "int"}}
-)
+    register_action('sleep', handle_sleep,
+                    args={"sleep_interval": {"required": True, "type": "int"}})
 
 
 def handle_sleep(input_data):
