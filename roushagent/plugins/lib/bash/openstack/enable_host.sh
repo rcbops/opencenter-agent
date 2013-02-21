2484f12f5f363c1ec5af3bee1c40197b9ccdea58
rackerjoe <breu@breu.org>
2013-02-21 14:38:12 -0600

    evacuate_host, disable_host, and enable_host

---
+++ roushagent/plugins/lib/bash/openstack/enable_host.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+source "$ROUSH_BASH_DIR/roush.sh"
+
+if [[ ! -e /root/openrc ]]; then
+    echo "/root/openrc does not exist and I cannot continue.  Good bye."
+    exit 1
+fi
+
+if [[ ! -e /usr/bin/nova-manage ]]; then
+    echo "nova-manage has not been installed and I cannot continue without it.  Good bye."
+    exit 2
+fi
+
+. /root/openrc
+
+nova-manage service enable --service=nova-compute --host=$(hostname -f)
+
+return_fact "maintenance_mode" "none"
