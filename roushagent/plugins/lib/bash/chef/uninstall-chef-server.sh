54f30b13f3da40051474ecbed64010b67955f099
wilk <william.kelly@rackspace.com>
2013-02-21 14:53:35 -0600

    remove facts when uninstalling chef server

--- roushagent/plugins/lib/bash/chef/uninstall-chef-server.sh
+++ roushagent/plugins/lib/bash/chef/uninstall-chef-server.sh
@@ -10,3 +10,6 @@ rm -rf /var/chef /var/log/chef-server/
 sed -i  '/export PATH=\${PATH}:\/opt\/chef-server\/bin/d' /root/.profile
 
 return_consequence "facts.backends := remove(facts.backends, 'chef-server')"
+for fact in chef_server_client_name chef_server_client_pem chef_server_uri chef_server_pem chef_server_cookbook_channels; do
+    return_fact "${fact}" "none"
+done
