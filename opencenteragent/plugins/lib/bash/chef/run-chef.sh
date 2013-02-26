a0c7f6889d510c7ce7d4a518fb53dc86c1e5bfc1
rpedde <ron@pedde.com>
2013-02-26 11:33:01 -0600

    clear converge attr on runs

--- opencenteragent/plugins/lib/bash/chef/run-chef.sh
+++ opencenteragent/plugins/lib/bash/chef/run-chef.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exec chef-client
+source "$OPENCENTER_BASH_DIR/opencenter.sh"
+
+if (! chef-client ); then
+    chef-client
+fi
+
+if [ $? -eq 0 ]; then
+    return_consequence "attrs.converged := true"
+else
+    return_consequence "attrs.converged := false"
+fi
