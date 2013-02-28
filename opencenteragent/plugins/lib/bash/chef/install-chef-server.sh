ada9ab0bf26e13c943598b1b53991bf313969a45
rackerjoe <breu@breu.org>
2013-02-28 13:34:35 -0600

    move locale-gen to ubuntu only section and lay down a log dir on startup if it doesn't exist

--- opencenteragent/plugins/lib/bash/chef/install-chef-server.sh
+++ opencenteragent/plugins/lib/bash/chef/install-chef-server.sh
@@ -34,9 +34,8 @@ id_OS
 
 CHEF_SERVER_VERSION=${CHEF_SERVER_VERSION:-11.0.4-1}
 
-locale-gen en_US.UTF-8
-
 if [[ $OS_TYPE = "debian"  ]] || [[ $OS_TYPE = "ubuntu" ]]; then
+    locale-gen en_US.UTF-8
     apt-get install -y --force-yes pwgen wget lsb-release
     cp /etc/resolv.conf /tmp/rc
     apt-get remove --purge resolvconf -y --force-yes
